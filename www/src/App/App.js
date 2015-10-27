454a975197f6b45859c3859ea17bb583f1303d16
Mathew Foscarini <mathew.foscarini@hellogbye.com>
2015-10-27 14:45:40 -0400

    fixed to run in chrome

---
+++ www/src/App/App.js
@@ -0,0 +1,43 @@
+(function (app) {
+
+    /**
+     * @name App
+     *
+     * @param {angular.ILogService} $log
+     * @param {angular.IScope} $rootScope
+     *
+     * @constructor
+     */
+    function App($log, $rootScope) {
+        $rootScope.$on('$stateChangeError',
+            function (event, toState, toParams, fromState, fromParams, error) {
+                var data = error && error.data;
+                $log.error('$stateChangeError: ' + toState.name);
+                $log.error(data);
+            });
+
+        $rootScope.$on('$stateChangeSuccess',
+            function (event, toState, toParams, fromState, fromParams) {
+                $log.debug('$stateChangeSuccess: ' + toState.name + ' ' + JSON.stringify(toParams));
+            });
+    }
+
+    app.run(['$log', '$rootScope', App]);
+
+    /**
+     * @name App.Config
+     *
+     * @param $locationProvider
+     *
+     * @constructor
+     */
+    function Config($locationProvider) {
+        $locationProvider.html5Mode(true);
+    }
+
+    app.config(['$locationProvider', Config]);
+
+})(angular.module('App', [
+    'ui.router',
+    'ngAssert'
+]));
